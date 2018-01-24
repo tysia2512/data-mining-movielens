923f606904eb947e602958dd8710fbdac6a7bf3c
ruda <tysia2512@gmail.com>
2018-01-24 17:53:32 +0100

    Fix this shit. Doesn't work

--- cf.py
+++ cf.py
@@ -1,6 +1,15 @@
-    display(ds.head(10))
 import numpy as np
 import scipy as sp
 
 def pearson(x, y):
-    return sp.stats.pearsonr(x, y)
+    if len(x.shape) == 1:
+        x = x.reshape((1, -1))
+    if len(y.shape) == 1:
+        y = y.reshape((1, -1))
+
+    prsn = np.zeros((x.shape[0], y.shape[0])
+    for xv in enumerate(x):
+        for yv in enumerate(y):
+            prsn[(xv[0], yv[0])] = sp.stats.pearsonr(xv[1], yv[1])
+    
+    return prsn
